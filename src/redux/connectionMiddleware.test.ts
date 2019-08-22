0fb59ffb89ed1c6a4cc5ac23b29b090e4f370abc
Tom Wright <talwrii@gmail.com>
2019-08-22 16:26:05 +0100

    Support aliases for names

---
+++ src/redux/connectionMiddleware.test.ts
@@ -0,0 +1,43 @@
+import { PvResolver, NoMapping } from "./connectionMiddleware";
+
+/* So a *resolution* can result in subscriptions
+
+*/
+
+it("maps correctly", (): void => {
+  let resolver = new PvResolver();
+  let actions = resolver.mapMacro("name", "dave");
+  let result = resolver.resolve("hello:${name}");
+  expect(result.pv.resolvedName).toBe("hello:dave");
+  expect(result.newResolutions.length).toBe(1);
+  expect(result.newResolutions[0].resolvedName).toBe("hello:dave");
+
+  let result2 = resolver.resolve("hello:${name}");
+  expect(result2.pv.resolvedName).toBe("hello:dave");
+  expect(result2.newResolutions.length).toBe(0);
+});
+
+it("supports remap", (): void => {
+  let resolver = new PvResolver();
+  let mapping1 = resolver.mapMacro("name", "dave");
+  expect(mapping1.newResolutions.length).toBe(0);
+  let result = resolver.resolve("hello:${name}");
+  expect(result.pv.resolvedName).toBe("hello:dave");
+
+  let mapping2 = resolver.mapMacro("name", "tim");
+  expect(mapping2.newResolutions.length).toBe(1);
+  expect(mapping2.newResolutions[0].resolvedName).toBe("hello:tim");
+  expect(mapping2.removedResolutions[0].resolvedName).toBe("hello:dave");
+
+  let resultTim = resolver.resolve("hello:${name}");
+  expect(resultTim.pv.resolvedName).toBe("hello:tim");
+  expect(resultTim.newResolutions.length).toBe(0);
+});
+
+it("missing mapping", (): void => {
+  let resolver = new PvResolver();
+  resolver.mapMacro("name", "dave");
+  expect(() => resolver.resolve("hello:${nam}")).toThrowError(
+    new NoMapping("nam")
+  );
+});
--- src/redux/connectionMiddleware.ts
+++ src/redux/connectionMiddleware.ts
@@ -26,6 +26,129 @@ function valueChanged(pvName: string, value: NType): void {
   });
 }
 
+class ResolvedPv {
+  public resolvedName: string;
+  public constructor(resolvedName: string) {
+    this.resolvedName = resolvedName;
+  }
+}
+
+export class NoMapping extends Error {
+  mapping: string;
+  constructor(public mapping: string) {
+    super("No mapping for " + mapping);
+    this.mapping = mapping;
+    this.name = "UnexpectedInput";
+  }
+}
+
+function interpolate(str: string, substitutions: { [string]: string }) {
+  let groups = [];
+
+  var result;
+  var regexp = /\${(.*?)}/g;
+  while ((result = regexp.exec(str))) {
+    groups.push(result[1]);
+  }
+  let requiredMappings = groups;
+  let missingsMappings = requiredMappings.filter(
+    x => substitutions[x] === undefined
+  );
+
+  if (missingsMappings.length != 0) {
+    throw new NoMapping(missingsMappings[0]);
+  }
+
+  return str.replace(/\${(.*?)}/g, (x, g) => substitutions[g]);
+}
+
+export class PvResolver {
+  substitutions: { [string]: string };
+  resolutions: { [unmapped: string]: ResolvedPv };
+
+  public constructor() {
+    this.substitutions = {};
+    this.resolutions = {};
+  }
+
+  private hasResolution(pv: ResolvedPv) {
+    const existingNames = Object.keys(this.resolutions).map(
+      x => x.resolvedName
+    );
+    return existingNames.indexOf(pv.resolvedName) != -1;
+  }
+
+  public resolve(
+    name: string
+  ): { pv: ResolvedPv; newResolutions: [ResolvedPv] } {
+    const oldResolution = this.resolutions[name];
+    const resolvedPv = new ResolvedPv(interpolate(name, this.substitutions));
+
+    console.log("resolve");
+    console.log(oldResolution);
+    console.log(resolvedPv);
+
+    var newResolutions;
+    if (
+      oldResolution == undefined ||
+      oldResolution.resolvedName != resolvedPv.resolvedName
+    ) {
+      console.log("New resolutiosn");
+      console.log(oldResolution);
+      console.log(resolvedPv);
+      newResolutions = [resolvedPv];
+    } else {
+      newResolutions = [];
+    }
+
+    this.resolutions[name] = resolvedPv;
+
+    return {
+      pv: resolvedPv,
+      newResolutions: newResolutions
+    };
+  }
+
+  public mapMacro(
+    macro: string,
+    valueString: string
+  ): { newResolutions: ResolvedPv[]; removedResolutions: ResolvedPv[] } {
+    /**
+       This may remove and add resolutions if the mappings have changed.
+     */
+    this.substitutions[macro] = valueString;
+
+    let newResolutions: ResolvedPv[] = [];
+    let removedResolutions: ResolvedPv[] = [];
+    let updates: { [unresolved: string]: ResolvedPv } = {};
+
+    for (let unresolved in this.resolutions) {
+      // potentially inefficient - we could pull out the mappings involved
+      //   in each resolution and then only remap these. But this feels like
+      //   a premature performance hack
+
+      let oldResolution = this.resolutions[unresolved];
+      let newResolution = new ResolvedPv(
+        interpolate(unresolved, this.substitutions)
+      );
+      if (oldResolution.resolvedName != newResolution.resolvedName) {
+        newResolutions.push(newResolution);
+        removedResolutions.push(oldResolution);
+        updates[unresolved] = newResolution;
+      }
+
+      for (let unresolved in updates) {
+        this.resolutions[unresolved] = updates[unresolved];
+      }
+    }
+
+    return {
+      newResolutions: newResolutions,
+      removedResolutions: removedResolutions
+    };
+  }
+}
+
 class ConnectionMiddleware {
   private connection: Connection;
 
